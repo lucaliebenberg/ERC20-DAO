f943e247c50234715ad3e47dcfe4568424d244f6
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-25 11:47:17 +0200

    added gov token + fix(bug)

---
+++ src/GovToken.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+
+contract GovToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20("GovToken", "GTK") ERC20Permit("GovToken") {}
+
+ function mintTo(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+
+}
